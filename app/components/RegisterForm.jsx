e81c63be78688b49070a8e146bd9e9a955551b19
varun <varunreddy2206@gmail.com>
2025-11-27 21:35:32 +0530

    userdashboard

--- app/components/RegisterForm.jsx
+++ app/components/RegisterForm.jsx
@@ -55,7 +55,7 @@ export default function RegistrationForm() {
             />
 
             {/* Register Button */}
-            <Link href="/otp">
+            <Link href="/userdashboard">
             <button className="w-full bg-blue-600 text-white py-3 rounded-md text-sm font-medium hover:bg-blue-700 transition">
               Register Now
             </button>
